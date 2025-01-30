9567af8eb5162c8fef6bc38e6fcf2b8a6890ccda
parvathisomanahallicnx <121179820+parvathisomanahallicnx@users.noreply.github.com>
2025-01-30 17:23:43 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -11,7 +11,7 @@ export default function decorate(block) {
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
-      const div = document.createElement('ul');
+      const div = document.createElement('div');
     });
     ul.append(li);
   });
